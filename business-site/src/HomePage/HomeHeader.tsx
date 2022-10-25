97e3545314a0b1567b5ac3eca4432751fade9f94
James Opara <jopara94@gmail.com>
2022-10-24 21:29:53 -0500

    test

--- business-site/src/HomePage/HomeHeader.tsx
+++ business-site/src/HomePage/HomeHeader.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import { Box, Container } from "@mui/material";
+import ShopImage from "../Images/ShopImage.jpg";
 
 export const HomeHeader: React.FunctionComponent = () => {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <Container maxWidth="xl" sx={{ display: "flex", flexDirection: "row" }}>
+      <Box
+        sx={{ background: { ShopImage }, width: "100%", height: "500px" }}
+      ></Box>
+      <Box sx={{ background: "green", width: "100%", height: "500px" }}></Box>
+    </Container>
+  );
 };
