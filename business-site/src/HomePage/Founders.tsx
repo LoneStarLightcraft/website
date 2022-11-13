a6b2ebbfa84c40ae6b22d3f2e81aad7028580a37
James Opara <jopara94@gmail.com>
2022-11-12 18:50:06 -0600

    James: Landing Page MVP done

---
+++ business-site/src/HomePage/Founders.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import GunGuy from "../Images/Gunguy.png";
+import Shades from "../Images/GlassesMuscles.png";
+import Boatman from "../Images/BoatJuan.png";
+import Shop from "../Images/ShopImage.png";
+
+export const Founders: React.FunctionComponent = () => {
+  const gridData = [
+    {
+      title: "Alec Gonzales",
+      caption: "CEO & Patron Saint of the 2nd Amendment",
+      image: GunGuy,
+    },
+    {
+      title: "Adan Rodriguez",
+      caption: "CFO & Patron Saint of the 2nd Amendment",
+      image: Shades,
+    },
+    {
+      title: "Juan Diaz",
+      caption: "CEO & Patron Saint of the 2nd Amendment",
+      image: Boatman,
+    },
+    {
+      title: "James Opara",
+      caption: "CTO & Enthusiast of Exotic Meats ",
+      image: Shop,
+    },
+  ];
+
+  return (
+    <Box
+      sx={{
+        background: "rgba(217, 176, 140, 0.5)",
+        backgroundSize: "cover",
+        width: "100%",
+        minHeight: "600px",
+        marginTop: 2,
+      }}
+    >
+      <Typography
+        variant="h2"
+        component="div"
+        sx={{
+          textTransform: "uppercase",
+          textAlign: "center",
+          fontFamily: "Oswald, sans-serif",
+          color: "#116466",
+          paddingTop: "8rem",
+          paddingBottom: "4rem",
+          paddingX: "1rem",
+        }}
+      >
+        Who we are
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          flexWrap: "wrap",
+        }}
+      >
+        {gridData.map((grid) => (
+          <Card
+            sx={{
+              margin: 5,
+              border: "none",
+              minWidth: "600px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              height="320"
+              image={grid.image}
+              alt="Paella dish"
+            />
+            <CardContent
+              sx={{
+                border: "none",
+                boxShadow: 0,
+                paddingBottom: 0,
+                paddingTop: 0,
+                transition: "background 250ms ease",
+
+                backgroundColor: "#000000",
+
+                "&:last-child": { padding: 0 },
+              }}
+            >
+              <Typography
+                sx={{
+                  textAlign: "center",
+                  fontSize: "1.7rem",
+                  fontFamily: "Oswald, sans-serif",
+                  color: "#d9b08c",
+                  paddingY: "2rem",
+                  paddingX: "1rem",
+                }}
+              >
+                <span style={{ fontWeight: 900 }}>{`${grid.title}`}</span>{" "}
+                <br />{" "}
+                <span style={{ fontWeight: 100 }}>{`${grid.caption}`}</span>
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
+    </Box>
+  );
+};
--- business-site/src/HomePage/HomeHeader.tsx
+++ business-site/src/HomePage/HomeHeader.tsx
@@ -1,14 +1,72 @@
 import React from "react";
-import { Box, Container } from "@mui/material";
-import ShopImage from "../Images/ShopImage.jpg";
+import { Box, Typography } from "@mui/material";
+import Logo from "../Images/LoneStar.png";
 
 export const HomeHeader: React.FunctionComponent = () => {
   return (
-    <Container maxWidth="xl" sx={{ display: "flex", flexDirection: "row" }}>
+    <>
       <Box
-        sx={{ background: { ShopImage }, width: "100%", height: "500px" }}
-      ></Box>
-      <Box sx={{ background: "green", width: "100%", height: "500px" }}></Box>
-    </Container>
+        sx={{
+          backgroundImage: `url(${Logo})`,
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          minHeight: "850px",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "27%",
+            left: "5%",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "4rem",
+              fontFamily: "Oswald, sans-serif",
+              color: "white",
+              textTransform: "uppercase",
+            }}
+          >
+            {"Houston-Based"} <br /> {"LED"}
+            <br />
+            {"Creations"}
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+          background: "white",
+          opacity: 0.95,
+          position: "fixed",
+          top: 0,
+          right: 0,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="div"
+          sx={{
+            textTransform: "uppercase",
+            fontSize: "1.7rem",
+            fontFamily: "Oswald, sans-serif",
+            fontWeight: "normal",
+            color: "#2c3531",
+            paddingY: "2rem",
+            paddingX: "1rem",
+          }}
+        >
+          Lone Star
+          <br /> Lightcraft
+        </Typography>
+      </Box>
+    </>
   );
 };
